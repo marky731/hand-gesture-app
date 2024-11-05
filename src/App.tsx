5ed12e3b2737b853419d2c6fd8838d9fef48909b
Chris <chris.dittinger@students.fhv.at>
2024-11-06 00:03:58 +0100

    progressbar with action trigger

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,51 @@ import { AnnotatedPrediction } from '@tensorflow-models/handpose';
 import './App.css';
 
 class Section {
-    x1: number;
-    y1: number;
-    x2: number;
-    y2: number;
-    iconImage: HTMLImageElement;
-
-    constructor(x1: number, y1: number, x2: number, y2: number, iconSrc: string) {
+    private _name: string;
+    private x1: number;
+    private y1: number;
+    private x2: number;
+    private y2: number;
+    private iconImage: HTMLImageElement;
+    private progress: number;
+    private debounceTimeout: NodeJS.Timeout | null;
+    private selectionStart: number | null = null;
+    private millisecondsTrigger: number = 1500;
+    private millisecondsDebounce: number = 500;
+    constructor(name: string, x1: number, y1: number, x2: number, y2: number, iconSrc: string) {
+        this._name = name;
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
         this.iconImage = new Image();
         this.iconImage.src = iconSrc;
+        this.progress = 0;
+        this.debounceTimeout = null;
     }
 
-    contains(x: number, y: number): boolean {
+    get name(): string {
+        return this._name;
+    }
+
+    public contains(x: number, y: number): boolean {
         return x >= this.x1 && x <= this.x2 && y >= this.y1 && y <= this.y2;
     }
 
-    getCenter(): { x: number, y: number } {
+    private getCenter(): { x: number, y: number } {
         return {
             x: (this.x1 + this.x2) / 2,
             y: (this.y1 + this.y2) / 2
         };
     }
 
-    draw(ctx: CanvasRenderingContext2D) {
+    public draw(ctx: CanvasRenderingContext2D) {
+        this.drawBorder(ctx);
+        this.drawIcon(ctx);
+        this.drawProgressRing(ctx);
+    }
+
+    private drawBorder(ctx: CanvasRenderingContext2D) {
         ctx.strokeStyle = 'red';
         ctx.lineWidth = 2;
         ctx.beginPath();
@@ -42,22 +60,22 @@ class Section {
         ctx.stroke();
     }
 
-    drawIcon(ctx: CanvasRenderingContext2D) {
+    private drawIcon(ctx: CanvasRenderingContext2D) {
         const center = this.getCenter();
-        const iconSize = 32; // Icon-Größe in Pixeln
-        const iconX = center.x - iconSize / 2; // Zentriert das Icon horizontal
-        const iconY = center.y - iconSize / 2; // Zentriert das Icon vertikal
+        const iconSize = 32;
+        const iconX = center.x - iconSize / 2;
+        const iconY = center.y - iconSize / 2;
 
         ctx.globalAlpha = 0.5;
         ctx.drawImage(this.iconImage, iconX, iconY, iconSize, iconSize);
         ctx.globalAlpha = 1.0;
     }
 
-    drawProgressRing(ctx: CanvasRenderingContext2D, progress: number) {
+    private drawProgressRing(ctx: CanvasRenderingContext2D) {
         const center = this.getCenter();
         const radius = 24;
         const startAngle = -Math.PI / 2;
-        const endAngle = startAngle + (progress / 100) * 2 * Math.PI;
+        const endAngle = startAngle + (this.progress / 100) * 2 * Math.PI;
 
         ctx.globalAlpha = 0.5;
         ctx.beginPath();
@@ -67,6 +85,44 @@ class Section {
         ctx.stroke();
         ctx.globalAlpha = 1.0;
     }
+
+    public resetProgress() {
+        this.selectionStart = null;
+        this.progress = 0;
+        if (this.debounceTimeout) {
+            clearTimeout(this.debounceTimeout);
+            this.debounceTimeout = null;
+        }
+    }
+
+    public progressing(): boolean{
+        if (this.debounceTimeout === null) {
+
+            if (this.selectionStart == null) {
+                this.selectionStart = Date.now();
+            } else {
+                
+                const timeDiff: number = Date.now() - this.selectionStart;
+                this.progress = Math.min(100, timeDiff / this.millisecondsTrigger * 100);
+
+                if (timeDiff > this.millisecondsTrigger) {
+                    
+
+                    this.debounceTimeout = setTimeout(() => {
+                        this.resetProgress();
+                    }, this.millisecondsDebounce);
+
+                    return true;
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+
+
 }
 
 const App: React.FC = () => {
@@ -74,8 +130,6 @@ const App: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [audioPlayer, setAudioPlayer] = useState<HTMLAudioElement | null>(null);
     const [currentSongIndex, setCurrentSongIndex] = useState<number>(0);
-    const [lastSection, setLastSection] = useState<number | null>(null);
-    const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
 
     const songs = [
         'song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3', 'song5.mp3',
@@ -83,11 +137,11 @@ const App: React.FC = () => {
     ];
 
     const sections = [
-        new Section(0, 0, 200, 240, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png'),
-        new Section(220, 0, 440, 240, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png'),
-        new Section(460, 0, 640, 240, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png'),
-        new Section(0, 250, 320, 480, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png'),
-        new Section(340, 250, 640, 480, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png')
+        new Section("LefTop",0, 0, 200, 240, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png'),
+        //new Section(220, 0, 440, 240, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png'),
+        new Section("RightTop",460, 0, 640, 240, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png'),
+        //new Section(0, 250, 320, 480, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png'),
+        //new Section(340, 250, 640, 480, 'https://cdn3.iconfinder.com/data/icons/player-ui-1/48/net-512.png')
     ];
 
     useEffect(() => {
@@ -113,7 +167,6 @@ const App: React.FC = () => {
             const ctx = canvasRef.current!.getContext('2d');
 
             let frameCount = 0;
-            let progress = 0;
 
             const detect = async () => {
                 if (video.readyState === video.HAVE_ENOUGH_DATA) {
@@ -123,59 +176,42 @@ const App: React.FC = () => {
                     ctx!.drawImage(video, 0, 0, canvasRef.current!.width, canvasRef.current!.height);
                     ctx!.setTransform(1, 0, 0, 1, 0, 0);
 
-                    // Zeichne jede Sektion, ihr Icon und den Fortschrittsring
                     sections.forEach((section) => {
                         section.draw(ctx!);
-                        section.drawIcon(ctx!);
-                        section.drawProgressRing(ctx!, progress);
                     });
-
-                    progress = (progress + 2) % 100;
 
                     if (frameCount % 10 === 0) {
                         const predictions: AnnotatedPrediction[] = await net.estimateHands(video);
-
                         if (predictions.length > 0) {
                             const hand = predictions[0].boundingBox;
-                            const handCenterX = (hand.topLeft[0] + hand.bottomRight[0]) / 2;
+                            const handCenterX = canvasRef.current!.width - ( (hand.topLeft[0] + hand.bottomRight[0]) / 2);
                             const handCenterY = (hand.topLeft[1] + hand.bottomRight[1]) / 2;
 
-                            const currentSectionIndex = sections.findIndex(section =>
-                                section.contains(handCenterX, handCenterY)
-                            );
-
-                            if (currentSectionIndex !== -1 && currentSectionIndex !== lastSection) {
-                                if (!debounceTimeout.current) {
-                                    console.log(`Hand in section ${currentSectionIndex}`);
-
-                                    if (currentSectionIndex === 0) {
-                                        playNextSong();
-                                    } else if (currentSectionIndex === 2) {
-                                        playPreviousSong();
+                            sections.forEach((section) => {
+                                if (section.contains(handCenterX, handCenterY)) {
+                                    if (section.progressing()) {
+                                        if (section.name == "LefTop") playPreviousSong();
+                                        if (section.name == "RightTop") playNextSong();
                                     }
-
-                                    debounceTimeout.current = setTimeout(() => {
-                                        debounceTimeout.current = null;
-                                    }, 1500);
-
-                                    setLastSection(currentSectionIndex);
+                                } else {
+                                    section.resetProgress();
                                 }
-                            }
+                            });
                         } else {
-                            setLastSection(null);
+                            sections.forEach((section) => section.resetProgress());
                         }
                     }
-                    frameCount++;
-                }
 
-                requestAnimationFrame(detect);
+                    frameCount++;
+                    requestAnimationFrame(detect);
+                }
             };
 
             detect();
         };
 
         detectHand();
-    }, [lastSection]);
+    }, []);
 
     const playAudio = (song: string) => {
         if (audioPlayer) {
